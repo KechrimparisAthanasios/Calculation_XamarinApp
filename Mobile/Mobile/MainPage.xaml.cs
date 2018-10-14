e705073cd389db67efa88d8ae560e8b0459fbb5f
Athanasios Kechrimparis <a.kechrimparis@outlook.com.gr>
2018-10-15 01:58:01 +0200

    Calculation Method ready/ Basic Keyboard

---
+++ Mobile/Mobile/MainPage.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mobile.ModelView;
+using Xamarin.Forms;
+
+namespace Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        ModelView.CalculationViewModel _calculationView;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = _calculationView = new CalculationViewModel();
+        }
+
+        private void SevenBtn_OnClicked(object sender, EventArgs e)
+        {
+            _calculationView.AddToFormula("7");
+        }
+
+        private void EightBtn_OnClicked(object sender, EventArgs e)
+        {
+            _calculationView.AddToFormula("8");
+        }
+
+        private void NineBtn_OnClicked(object sender, EventArgs e)
+        {
+            _calculationView.AddToFormula("9");
+        }
+
+        private void FourBtn_OnClicked(object sender, EventArgs e)
+        {
+            _calculationView.AddToFormula("4");
+        }
+
+        private void FiveBtn_OnClicked(object sender, EventArgs e)
+        {
+            _calculationView.AddToFormula("5");
+        }
+
+        private void SixBtn_OnClicked(object sender, EventArgs e)
+        {
+            _calculationView.AddToFormula("6");
+        }
+
+        private void OneBtn_OnClicked(object sender, EventArgs e)
+        {
+            _calculationView.AddToFormula("1");
+        }
+
+        private void TwoBtn_OnClicked(object sender, EventArgs e)
+        {
+            _calculationView.AddToFormula("2");
+        }
+
+        private void ThreeBtn_OnClicked(object sender, EventArgs e)
+        {
+            _calculationView.AddToFormula("3");
+        }
+
+        private void CommaBtn_OnClicked(object sender, EventArgs e)
+        {
+            _calculationView.AddToFormula(".");
+        }
+
+        private void ZeroBtn_OnClicked(object sender, EventArgs e)
+        {
+            _calculationView.AddToFormula("0");
+        }
+
+        private void CancelBtn_OnClicked(object sender, EventArgs e)
+        {
+            _calculationView.AddToFormula("C");
+        }
+
+        private void DividedBtn_OnClicked(object sender, EventArgs e)
+        {
+            _calculationView.AddToFormula("/");
+        }
+
+        private void MultipleBtn_OnClicked(object sender, EventArgs e)
+        {
+            _calculationView.AddToFormula("*");
+        }
+
+        private void MinusBtn_OnClicked(object sender, EventArgs e)
+        {
+            _calculationView.AddToFormula("-");
+        }
+
+        private void PlusBtn_OnClicked(object sender, EventArgs e)
+        {
+            _calculationView.AddToFormula("+");
+        }
+
+        private void EqualBtn_OnClicked(object sender, EventArgs e)
+        {
+            _calculationView.Calculate();
+        }
+
+        private void AllClearBtn_OnClicked(object sender, EventArgs e)
+        {
+            _calculationView.AddToFormula("AC");
+        }
+    }
+}
---
+++ Mobile/Mobile/ModelView/CalculationViewModel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Mobile.ModelView
+{
+    public class CalculationViewModel : INotifyPropertyChanged
+    {
+        #region Properties
+
+        private string _formulaLbl;
+
+        public string FormulaLbl
+        {
+            get { return _formulaLbl; }
+            set
+            {
+                _formulaLbl = value;
+                NotifyPropertyChanged("FormulaLbl");
+            }
+        }
+
+        private string _result;
+
+        public string Result
+        {
+            get { return _result;}
+            set { _result = value; NotifyPropertyChanged("Result"); }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public CalculationViewModel()
+        {
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void AddToFormula(string value)
+        {
+            switch (value)
+            {
+                case "AC":
+                    if (!String.IsNullOrEmpty(FormulaLbl))
+                    {
+                        FormulaLbl = "";
+                    }
+
+                    break;
+                case "C":
+                    if (!String.IsNullOrEmpty(FormulaLbl))
+                    {
+                        FormulaLbl = FormulaLbl.Remove(FormulaLbl.Length - 1);
+                    }
+
+                    break;
+                default:
+                    FormulaLbl = FormulaLbl + value;
+                    break;
+            }
+        }
+
+        public void Calculate()
+        {
+            try
+            {
+                org.mariuszgromada.math.mxparser.Expression calculation;
+                calculation = new org.mariuszgromada.math.mxparser.Expression(FormulaLbl); //5*60
+                Result = calculation.calculate().ToString(); //300
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        #endregion
+
+        #region INotify Event
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
